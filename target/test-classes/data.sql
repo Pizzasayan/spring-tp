bab6a1fc8e8865a1cefc2eebff7ba642e00422c0
Farda Safari <fardasafari@gmail.com>
2024-10-16 09:15:23 +0200

    clean articles

--- target/test-classes/data.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS article (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(250) NOT NULL,
-    email VARCHAR(250) NOT NULL,
-    password VARCHAR(250) NOT NULL
-);
-INSERT INTO users (username, email, password) VALUES
-    ('Guillaume', 'guillaume.nagiel@gmail.com', 'guillaume'),
-    ('test','test.test@test.com', 'test');
